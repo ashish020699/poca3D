f825bec187e2c896014aa7bf7ee3f35f92cb2a1e
ashish020699 <59076354+ashish020699@users.noreply.github.com>
2020-01-03 15:01:50 +0530

    Delete midpoint.py

--- midpoint.py
+++
@@ -1,8 +0,0 @@
-def midpoint(x1,y1,z1,x2,y2,z2,x3,y3,z3):
- x=(x1+x2+x3)/3 
- y=(y1+y2+y3)/3
- z=(z1+z2+z3)/3
- print ("x:",x)
- print ("y:",y)
- print ("z:",z)
-midpoint(1.3,8.8,3.4,0.1,2.8,.53,1,5.2,9.3)
